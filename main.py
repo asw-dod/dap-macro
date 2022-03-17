9e9fbf5e155033ebf7a9b39d069d91152a3c6574
Piorosen <aoikazto@gmail.com>
2022-03-17 18:16:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ repository_name = "dap-macro"
 options = webdriver.ChromeOptions()
 options.add_argument("start-maximized")
 options.add_argument("lang=ko_KR")
-# options.add_argument('headless')
+options.add_argument('headless')
 options.add_argument('window-size=1920x1080')
 options.add_argument("disable-gpu")
 options.add_argument("--no-sandbox")
